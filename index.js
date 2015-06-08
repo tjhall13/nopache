f67a0664052b7bbfeb697349830db5d3825465db
thall3 <thall3@unl.edu>
2015-06-08 03:54:11 -0500

    Rearranged exports and added url check to index.html files

--- index.js
+++ index.js
@@ -3,93 +3,119 @@ var url = require('url');
 var path = require('path');
 var fs = require('fs');
 
-function NopacheServer(rootDir, port) { 
-    if(rootDir.charAt(0) == '~') {
-        rootDir = path.join(process.env.HOME || process.env.HOMEPATH, rootDir.substr(1));
-    }
-    var pathname = path.resolve(rootDir);
-    
-    function openRequest(request, callback) {
-        var filename = path.join(pathname, request);
-        var stat;
-        
-        try {
-            stat = fs.statSync(filename);
-        } catch(err) {
-            callback('url does not exist', null);
-            return false;
+module.exports = {
+    NopacheServer: function(base, port) { 
+        if(base.charAt(0) == '~') {
+            base = path.join(process.env.HOME || process.env.HOMEPATH, base.substr(1));
         }
+        var pathname = path.resolve(base);
         
-        if(stat) {
-            if(stat.isDirectory()) {
-                filename = path.join(filename, 'index.html');
+        function openRequest(request, callback) {
+            var filename = path.join(pathname, request);
+            var stat;
+            
+            try {
                 stat = fs.statSync(filename);
-                
-                if(!stat || !stat.isFile()) {
-                    callback('url does not exist', null);
-                    return false;
-                }
-            } else if(!stat.isFile()) {
+            } catch(err) {
                 callback('url does not exist', null);
                 return false;
             }
             
-            var extension = path.extname(filename);
-            var type;
-            
-            switch(extension) {
-                case '.html':
-                    type = 'text/html';
-                    break;
-                case '.css':
-                    type = 'text/css';
-                    break;
-                case '.js':
-                    type = 'text/javascript';
-                    break;
-                case '.jpeg':
-                    type = 'image/jpeg';
-                    break;
-                default:
-                    type = undefined;
-            }
-            
-            var header = { };
-            
-            fs.readFile(filename, function(err, data) {
-                header['Content-Length'] = data.length;
-                if(type) {
-                    header['Content-Type'] = type;
+            if(stat) {
+                if(stat.isDirectory()) {
+                    filename = path.join(filename, 'index.html');
+                    
+                    try {
+                        stat = fs.statSync(filename);
+                    } catch(err) {
+                        callback('url does not exist', null);
+                        return false;
+                    }
+                    
+                    if(!stat || !stat.isFile()) {
+                        callback('url does not exist', null);
+                        return false;
+                    }
+                } else if(!stat.isFile()) {
+                    callback('url does not exist', null);
+                    return false;
                 }
                 
-                callback(err, {
-                    header: header,
-                    body: data
+                var extension = path.extname(filename);
+                var type;
+                
+                switch(extension) {
+                    case '.html':
+                        type = 'text/html';
+                        break;
+                    case '.css':
+                        type = 'text/css';
+                        break;
+                    case '.js':
+                        type = 'text/javascript';
+                        break;
+                    case '.jpeg':
+                        type = 'image/jpeg';
+                        break;
+                    default:
+                        type = undefined;
+                        break;
+                }
+                
+                var header = { };
+                
+                fs.readFile(filename, function(err, data) {
+                    header['Content-Length'] = data.length;
+                    if(type) {
+                        header['Content-Type'] = type;
+                    }
+                    
+                    callback(err, {
+                        header: header,
+                        body: data
+                    });
                 });
-            });
-        } else {
-            callback('url does not exist', null);
-            return false;
-        }
-    }
-    
-    var server = http.createServer(function(request, response) {
-        var requestURL = url.parse(request.url);
-        openRequest(requestURL.pathname, function(err, data) {
-            if(err) {
-                response.writeHead(404);
-                response.end();
             } else {
-                response.writeHead(200, data.header);
-                response.write(data.body);
-                response.end();
+                callback('url does not exist', null);
+                return false;
             }
+        }
+        
+        var server = http.createServer(function(request, response) {
+            var requestURL = url.parse(request.url);
+            openRequest(requestURL.pathname, function(err, data) {
+                if(err) {
+                    response.writeHead(404);
+                    response.end();
+                } else {
+                    response.writeHead(200, data.header);
+                    response.write(data.body);
+                    response.end();
+                }
+            });
         });
-    });
-    
-    this.listen = function() {
-        server.listen(port);
+        
+        this.listen = function() {
+            server.listen(port);
+        }
+    },
+    config: function(config) {
+        // Package specific defaults
+        var defaults = {
+            base: '.',
+            port: 2400
+        };
+        
+        // Merge options into defaults
+        for(var option in config) {
+            defaults[option] = config[option];
+        }
+        
+        return defaults;
+    },
+    cli: function(config) {
+        // Create a simple server with the provided configuration
+        var server = new NopacheServer(config.base, config.port);
+        server.listen();
     }
-}
-
-module.exports = { nopache: NopacheServer };
+};
