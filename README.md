7b13d5e1481a22d1616a4a993a75b9f6bbcb3a06
thall3 <thall3@unl.edu>
2015-10-02 02:13:54 -0500

    Finalized beta plugin api

---
+++ README.md
@@ -0,0 +1,153 @@
+Nopache
+=======
+
+Install:
+npm install nopache
+
+Node webserver that can simulate php requests.  Eventually nohp (A node php interpreter) will be incorportated into nopache to allow php files to be ran as they would on a full web server.
+
+# Usage
+## Nopache Server
+```javascript
+// Include NopacheServer in your project
+var NopacheServer = require('nopache').NopacheServer;
+
+// Create a new server instance
+var server = new NopacheServer({
+    base: String, // *required*
+            /* Path that will be the server document root   */
+    port: Number, // *required*
+            /* Port number the server will listen on
+    override: Boolean, // default: false
+            /* Allow .htaccess files to be read             */
+    logfile: String, // default: null
+            /* Sets the path of the error log file          */
+            /* Accepts 1, 2, 'stdout' and 'stderr'          */
+            /* as special values                            */
+    ssl: {  // default: false
+        key: String, // *required*
+            /* path to ssl key file (.pem)                  */
+        cert: String, // *required*
+            /* path to ssl certificate file (.crt)          */
+    }
+});
+
+// Start web server
+server.listen();
+
+// Close web server
+server.close();
+```
+
+## Plugin Interface
+With Nopache v0.9.0 and later, anyone can write a simple plugin for the Nopache webserver.  The plugin has access to the request and response data at every stage of the server.  To use a custom plugin, use the path to the plugin as the key in the mods key-value pairs or install a plugin with npm. Plugins installed by npm must follow the naming convention mod_MYPLUGIN where MYPLUGIN is the name provided to the mods object parameter.
+
+### Plugin Creation
+To create a plugin just make a js file or node package that when imported exposes any of the 5 functions to register hooks. Those functions are:
+
+-   ```register_initialize_hook(config, arg)```
+-   ```register_request_hook(config)```
+-   ```register_access_hooks(config)```
+-   ```register_process_hooks(config)```
+-   ```register_response_hook(config)```
+
+Each function will be provided the configuration variable used to configure the server and the initialize function is given a special argument which is the value in the mods key-value pairs.
+
+#### ```register_initialize_hook(config, arg)```
+Called at the start up of the server to initialize the plugin.  Is given the value in the mod declaration as the arg parameter.
+
+#### ```register_request_hook(config)```
+This function must return a function with the signature: ```handler(env, callback)``` where env is the current request environment with relevant data. callback *MUST* be called as ```callback(err, data)```.  If err is not null, the server will respond with error 500.  The data parameter must be the env variable.
+
+#### ```register_access_hooks(config)```
+This function must return a key-value map of htaccess file commands and handlers.  Each handler will get called when the coresponding command is given in an htaccess file with the env parameter.  ```this``` will be set to a separate context for each module that is mutable for the lifetime of the request.
+
+#### ```register_process_hooks(config)```
+This function must return an array of objects with the key ```regex```; value equal to a ```RegExp``` and with the key ```handler```; value equal to a function with the signature ```function(env, callback)```.  Example:
+```javascript
+{
+    regex: new RegExp('^test.+regex$'),
+    value: function(env, callback) {
+        ...
+        callback(null, env);
+        return true;
+    }
+}
+
+```
+This function must return true if it chooses to process the request otherwise it *MUST* return false.  If it does handle the request it must call the callback with the same rules as the ```request_hook```.
+
+#### ```register_response_hook(config)```
+This must return a function to which will be called after the request has been processed.  The rules for the returned handler is identical to ```register_request_hook```.
+
+## Contributed Plugins
+### ```mock```
+Mock provides an easy way to mock entire requests.  It simply requires an object or path to a file to include as an object for the mock interface.  Mocks can be static, selected from an array of possible responses based on the request data or a simple function that returns the relevant data.
+
+#### Mock Interface
+```javascript
+// Create a static response
+var scripts = {
+    '/example.php': {
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        data: 'Hello, World!'
+    }
+};
+
+// Create an array of responses depending on request data
+var scripts = {
+    '/example.php': [
+        {
+            input: {
+                get: { foo: 'value' }
+            },
+            output: {
+                headers: {
+                    'Content-Type': 'text/plain'
+                },
+                data: 'Hello'
+            }
+        },
+        {
+            input: {
+                get: { bar: 'other' }
+            },
+            output: {
+                headers: {
+                    'Content-Type': 'text/plain'
+                },
+                data: 'World'
+            }
+        },
+    ]
+};
+
+// Create a response that calls a javascript function
+var scripts = {
+    '/example.php': function(input) {
+        // input == { get: { foo: 'bar' }, post: { } ... }
+        var output = {
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            data: 'Hello, World!'
+        };
+        return output;
+    }
+};
+
+// Create server instance using mock
+var config = {
+    ...
+};
+
+var server = new NopacheServer(config, {
+    mock: script
+};
+```
+
+# License
+
+MIT
--- contrib/mod_mock/index.js
+++ contrib/mod_mock/index.js
@@ -4,105 +4,112 @@ var Buffer = require('buffer').Buffer;
 var request = require('../lib/request.js');
 var pkg = require('./package.json');
 
-function handle_object(object, env, callback) {
-    env.response.status(200);
-    env.response.headers(object.headers);
-    switch(typeof object.data) {
-        case 'string':
-            env.response.data(new Buffer(object.data));
-            break;
-        case 'object':
-        if(Buffer.isBuffer(object.data)) {
-            env.response.data(object.data);
-        } else {
-            env.response.headers({ 'Content-Type': 'application/json' }, true);
-            env.response.data(new Buffer(JSON.stringify(object.data)));
+function Mock() {
+    var hooks = { };
+    
+    function handle_object(object, env, callback) {
+        env.response.status(200);
+        env.response.headers(object.headers);
+        switch(typeof object.data) {
+            case 'string':
+                env.response.data(new Buffer(object.data));
+                break;
+            case 'object':
+            if(Buffer.isBuffer(object.data)) {
+                env.response.data(object.data);
+            } else {
+                env.response.headers({ 'Content-Type': 'application/json' }, true);
+                env.response.data(new Buffer(JSON.stringify(object.data)));
+            }
+                break;
+            default:
+                break;
         }
-            break;
-        default:
-            break;
+        callback(null, env);
+        return true;
     }
-    callback(null, env);
-    return true;
-}
 
-function handle_string(str, env, callback) {
-    var response = {
-        headers: {
-            'Content-Type': 'text/plain',
-            'Content-Length': str.length
-        },
-        data: new Buffer(str)
+    function handle_string(str, env, callback) {
+        var response = {
+            headers: {
+                'Content-Type': 'text/plain',
+                'Content-Length': str.length
+            },
+            data: new Buffer(str)
+        };
+        
+        return handle_object(response, env, callback);
+    }
+
+    function handle_array(arr, data, env, callback) {
+        arr.forEach(function(value) {
+            if(request.compare(value.request, data)) {
+                return handle_object(value.response, env, callback);
+            }
+        });
+        
+        env.response.status(404);
+        callback(null, env);
+        return false;
+    }
+
+    function handle_function(func, data, env, callback) {
+        var response = func.call(env, data);
+        return handle_object(response, env, callback);
+    }
+
+    function handle_request(mock, env, callback) {
+        var requestData = request.parse(env);
+        switch(typeof mock) {
+            case 'function':
+                handle_function(mock, requestData, env, callback);
+                break;
+            case 'object':
+            if(Array.isArray(mock)) {
+                handle_array(mock, requestData, env, callback);
+            } else {
+                handle_object(mock, env, callback);
+            }
+                break;
+            case 'string':
+                handle_string(mock, env, callback);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+    
+    this.register_initialize_hook = function(config, arg) {
+        var mocks;
+        var ext;
+        
+        if(typeof arg == 'string') {
+            try {
+                mocks = require(path.resolve(arg));
+            } catch(e) {
+                console.error(e);
+            }
+        } else if(typeof arg == 'object') {
+            mocks = arg;
+        }
+        var handler = function(env, callback) {
+            var mock = mocks[env.request.path];
+            if(mock) {
+                return handle_request(mock, env, callback);
+            } else {
+                return false;
+            }
+        };
+        for(var file in mocks) {
+            ext = path.extname(file);
+            if(!hooks[ext]) {
+                hooks[ext] = handler;
+            }
+        }
     };
     
-    return handle_object(response, env, callback);
-}
-
-function handle_array(arr, data, env, callback) {
-    arr.forEach(function(value) {
-        if(request.compare(value.request, data)) {
-            return handle_object(value.response, env, callback);
-        }
-    });
-    
-    env.response.status(404);
-    callback(null, env);
-    return false;
-}
-
-function handle_function(func, data, env, callback) {
-    var response = func.call(env, data);
-    return handle_object(response, env, callback);
-}
-
-function handle_request(mock, env, callback) {
-    var requestData = request.parse(env);
-    switch(typeof mock) {
-        case 'function':
-            handle_function(mock, requestData, env, callback);
-            break;
-        case 'object':
-        if(Array.isArray(mock)) {
-            handle_array(mock, requestData, env, callback);
-        } else {
-            handle_object(mock, env, callback);
-        }
-            break;
-        case 'string':
-            handle_string(mock, env, callback);
-            break;
-        default:
-            return false;
-    }
-    return true;
-}
-
-var hooks = { };
-
-module.exports = {
-    register_initialize_hook: function(config, arg) {
-        try {
-            var mocks = require(path.resolve(arg));
-            var ext;
-            var handler = function(env, callback) {
-                var mock = mocks[env.request.path];
-                if(mock) {
-                    return handle_request(mock, env, callback);
-                } else {
-                    return false;
-                }
-            };
-            for(var file in mocks) {
-                ext = path.extname(file);
-                if(!hooks[ext]) {
-                    hooks[ext] = handler;
-                }
-            }
-        } catch(e) {
-            console.error(e);
-        }
-    },
-    register_process_hooks: function() {
+    this.register_process_hooks = function() {
         var handlers = [];
         for(var ext in hooks) {
             handlers.push({
@@ -111,9 +118,11 @@ module.exports = {
             });
         }
         return handlers;
-    },
+    };
     
-    version: function() {
+    this.version = function() {
         return pkg.version;
-    }
-};
+    };
+}
+
+module.exports = new Mock();
